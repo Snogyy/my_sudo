4b9d4aa543607fdfadda63a65ade42efe3ef6305
Esteban Munier <esteban.munier@epitech.eu>
2025-01-28 18:07:04 +0100

    feat: test for flo

--- get_user.c
+++ get_user.c
@@ -9,36 +9,18 @@
 
 char *get_user(void)
 {
-    int fd = open(PASSWD, O_RDONLY);
-    struct stat sd;
-    char **tab = NULL;
-    char *temp = NULL;
-    int nDigits = 0;
-    char *rep = NULL;
-    char *buffer = NULL;
+    FILE *fd;
+    char buff[512];
+    char asker_name[256];
+    char pass[256];
+    int asker_uid;
 
-    if (!fd)
-        return NULL;
-    stat(PASSWD, &sd);
-    buffer = malloc(sizeof(char) * sd.st_size + 1);
-    if (!buffer)
-        return NULL;
-    read(fd, buffer, sd.st_size);
-    buffer[sd.st_size] = '\0';
-    tab = separate(buffer);
-    for (int i = 0; tab; i++) {
-        nDigits = floor(log10(abs((int)getuid()))) + 1;
-        temp = malloc(sizeof(char) * nDigits + 1);
-        sprintf(temp, "%d", (int)getuid());
-        temp[nDigits] = '\0';
-        if (i % 7 == 0 && strcmp(temp, tab[i]) == 0) {
-            rep = strdup(tab[i - 3]);
+    fd = fopen("/etc/passwd", "r");
+    while (fgets(buff, sizeof(buff), fd)) {
+        sscanf(buff, "%31[^:]:%31[^:]:%d:", asker_name, pass, &asker_uid);
+        if (asker_uid == getuid())
             break;
-        }
-        free(temp);
     }
-    free(temp);
-    free(buffer);
-    close(fd);
-    return rep;
+    fclose(fd);
+    return strdup(asker_name);
 }
